7899e26c175e9addeec69b32bed95b1a47d37d24
Thibaud Navarre <navarre.thibaud@gmail.com>
2022-06-20 19:03:52 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,26 @@
-import { defineConfig } from "vite";
-import { getMapsOptimizers, getMapsScripts } from "wa-map-optimizer-vite";
+import { defineConfig } from 'vite';
+import { getMapsOptimizers, getMapsScripts } from 'wa-map-optimizer-vite';
 
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: {
-                index: "./index.html",
-                ...getMapsScripts(),
-            },
-        },
+  build: {
+    rollupOptions: {
+      input: {
+        index: './index.html',
+        ...getMapsScripts(),
+      },
     },
-    plugins: [...getMapsOptimizers()],
-    server: {
-        host: "localhost",
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
-            "Cache-Control": "no-cache, no-store, must-revalidate",
-        },
-        open: "/",
+  },
+  plugins: [...getMapsOptimizers()],
+  server: {
+    host: 'localhost',
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Private-Network': 'true',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers':
+        'X-Requested-With, content-type, Authorization',
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
     },
+    open: '/',
+  },
 });
